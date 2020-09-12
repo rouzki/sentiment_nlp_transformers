5b9720f490168c550866b260349c24229b0070b8
zakarya <zakaryarouzki@gmail.com>
2020-09-12 17:02:29 +0000

    adding forward and validation epochs

--- demo.py
+++ demo.py
@@ -9,104 +9,117 @@ import transformers
 
 import sh
 
-sh.rm('-r', '-f','logs')
-sh.mkdir('logs') 
+sh.rm("-r", "-f", "logs")
+sh.mkdir("logs")
 
-flags.DEFINE_boolean('debug', False, '')
-flags.DEFINE_integer('epochs', 10, '')
-flags.DEFINE_float('lr', 1e-2, '')
-flags.DEFINE_float('momentum', .9, '')
-flags.DEFINE_string('model', 'bert-base-uncased', '')
-flags.DEFINE_integer('seq_length', 32, '')
-flags.DEFINE_integer('batch_size', 16, '')
+flags.DEFINE_boolean("debug", False, "")
+flags.DEFINE_integer("epochs", 10, "")
+flags.DEFINE_float("lr", 1e-2, "")
+flags.DEFINE_float("momentum", 0.9, "")
+flags.DEFINE_string("model", "bert-base-uncased", "")
+flags.DEFINE_integer("seq_length", 32, "")
+flags.DEFINE_integer("batch_size", 16, "")
 FLAGS = flags.FLAGS
-
 
 
 class IMDBSentimentClassifier(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.model = transformers.BertForSequenceClassification.from_pretrained(FLAGS.model)
+        self.model = transformers.BertForSequenceClassification.from_pretrained(
+            FLAGS.model
+        )
+        self.loss = th.nn.CrossEntropyLoss(reduction = 'none')
+        self.train_ds = self._prepare_ds('train')
+        self.test_ds = self._prepare_ds('test')
+
 
     def prepare_data(self):
-        train_ds = nlp.load_dataset('imdb', split=f'train[:{FLAGS.batch_size if FLAGS.debug else "5%"}]')
+        pass
+        # train_ds = nlp.load_dataset(
+        #     "imdb", split=f'train[:{FLAGS.batch_size if FLAGS.debug else "5%"}]'
+        # )
+        
+        # def _tokenize(x):
+        #     x["input_ids"] = tokenizer.encode(
+        #         x["text"], 
+        #         pad_to_max_length=True,
+        #         max_length=FLAGS.seq_length,
+        #     )
+        #     return x
+
+        # train_ds = train_ds.map(_tokenize)
+        # train_ds.set_format(type="torch", columns=["input_ids", "label"])
+
+
+
+    def _prepare_ds(self, split):
+        ds = nlp.load_dataset(
+            "imdb", split=f'{split}[:{FLAGS.batch_size if FLAGS.debug else "5%"}]'
+        )
         tokenizer = transformers.BertTokenizer.from_pretrained(FLAGS.model)
         def _tokenize(x):
-            x['input_ids'] = tokenizer.encode(
-                x['text'],
-                max_length=FLAGS.seq_length,
-                padding=True
+            x["input_ids"] = tokenizer.encode(
+                x["text"],
+                max_length = FLAGS.seq_length,
             )
             return x
-        train_ds = train_ds.map(_tokenize)
-        train_ds.set_format(type='torch', columns=['input_ids', 'label'])
-        import IPython; IPython.embed(); exit(1)
-        
-    def _tokenize(x):
-        import IPython; IPython.embed(); exit(1)
-        
-        x['input_ids'] = tokenizer.encode(
-            x['text'],
-            max_length=FLAGS.seq_length,
-            
-        )
-
-    def _prepare_ds(split):        
-        ds = nlp.load_dataset('imdb', split=f'{split}[:{FLAGS.batch_size if FLAGS.debug else "5%"}]')
         ds = ds.map(_tokenize)
-        ds.set_format(type='torch', columns=['input-ids', 'label'])
+        ds.set_format(type="torch", columns=["input_ids", "label"])
         return ds
-    
+
     # self.train_ds, self.test_ds = map(_prepare_ds, ('train', 'test'))
 
+    def forward(self, input_ids):
+        mask = (input_ids != 0).float()
+        logits, = self.model(input_ids, mask)
+        return logits
 
-    def forward(self, batch):
-        import IPython; IPython.embed(); exit(1)
-        
+    def validation_step(self, batch, batch_idx):
+        logits = self.forward(batch['input_ids'])
+        loss = self.loss(logits, batch['label'])
+        acc = (logits.argmax(-1) == batch['label']).float()
+        return { 'loss' : loss, 'acc': acc }
 
     def training_step(self, batch, batch_idx):
         import IPython; IPython.embed(); exit(1)
-        
-    def validation_epoch_end(self, ouputs):
-        import IPython; IPython.embed(); exit(1)
-        
 
-    def validation_step(self, batch, batch_idx):
-        pass
+    def validation_epoch_end(self, ouputs):
+        losses = th.cat([ o['loss'] for o in outputs], 0).mean()
+        acc = th.cat([ o['acc'] for o in outputs], 0).mean()
+        out = {'val_loss': loss, 'val_acc': acc}
+        return {**out, 'log': out}
 
     def train_dataloader(self):
         return th.utils.data.DataLoader(
             self.train_ds,
-            batch_size = FLAGS.batch_size,
-            drop_last = True,
-            shuffle = True,
+            batch_size=FLAGS.batch_size,
+            drop_last=True,
+            shuffle=True,
         )
+
     def val_dataloader(self):
         return th.utils.data.DataLoader(
-            self.train_ds,
-            batch_size = FLAGS.batch_size,
-            drop_last = False,
-            shuffle = False,
+            self.test_ds,
+            batch_size=FLAGS.batch_size,
+            drop_last=False,
+            shuffle=False,
         )
 
     def configure_optimizers(self):
-        return th.optim.SGD(
-            self.parameters(),
-            lr=FLAGS.lr,
-            momentum=FLAGS.momentum
-        )
+        return th.optim.SGD(self.parameters(), lr=FLAGS.lr, momentum=FLAGS.momentum)
 
 
 def main(_):
     model = IMDBSentimentClassifier()
     trainer = pl.Trainer(
-        default_root_dir='logs',
+        default_root_dir="logs",
         gpus=(1 if th.cuda.is_available() else 0),
-        max_epochs = FLAGS.epochs,
-        fast_dev_run=FLAGS.debug
+        max_epochs=FLAGS.epochs,
+        fast_dev_run=FLAGS.debug,
     )
     trainer.fit(model)
     logging.info("hello")
+
 
 if __name__ == "__main__":
     app.run(main)
